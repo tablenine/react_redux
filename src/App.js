969872d68dbe78b7298e0a956338754c7531b167
tablenine <tablenine@naver.com>
2022-05-17 11:07:51 +0900

    - redux 없이 사용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
 import './App.css';
+import DisplayNumberRoot from "./component/DisplayNumberRoot";
+import AddNumberRoot from "./component/AddNumberRoot";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {number: 10}
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Root</h1>
+        <AddNumberRoot onClick={function (size) {
+          this.setState({number: this.state.number + size})
+        }.bind(this)}/>
+        <DisplayNumberRoot number={this.state.number}/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
